56516bd6ccc7e0578289fd62ba5c071457b5b3ff
Aleksey Pastukhov <mindflux99@gmail.com>
2024-12-16 16:06:50 +0200

    Solution1

--- app/cafe.py
+++ app/cafe.py
@@ -17,16 +17,16 @@ class Cafe:
         expiration_date = vaccine_info.get("expiration_date")
 
         if not expiration_date:
-            raise OutdatedVaccineError(f'{visitor.get("name", "Visitor")}\'s '
+            raise OutdatedVaccineError(f"{visitor.get("name", "Visitor")}\'s "
                                        f"vaccine information is incomplete.")
 
         if expiration_date < datetime.date.today():
-            raise OutdatedVaccineError(f'{visitor.get("name", "Visitor")}\'s '
+            raise OutdatedVaccineError(f"{visitor.get("name", "Visitor")}\'s "
                                        f"vaccine is expired.")
 
         # Check mask-wearing status
         if not visitor.get("wearing_a_mask", False):
-            raise NotWearingMaskError(f'{visitor.get("name", "Visitor")} '
+            raise NotWearingMaskError(f"{visitor.get("name", "Visitor")} "
                                       f"is not wearing a mask.")
 
         return f"Welcome to {self.name}"
